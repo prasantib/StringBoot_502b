11d672f197dcd092521803c905256f22aa56c46c
prasantib <prasanti.babu@gmail.com>
2019-10-10 10:24:47 -0400

    StringBoot_502

---
+++ src/main/java/com/example/demo/StringBoot502Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StringBoot502Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StringBoot502Application.class, args);
+    }
+
+}
